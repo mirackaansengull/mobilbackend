bce459b26fdde4cfe86508161a90216359d77927
Miraç Kaan Şengül <mirac.kaansengull@gmail.com>
2025-12-12 17:45:08 +0300

    go versiyon uyumsuzluğu

--- database.go
+++ database.go
@@ -9,26 +9,21 @@ import (
 )
 
 func main() {
-	// 1. Context oluştur
 	ctx := context.Background()
 
 	sa := option.WithCredentialsFile("/etc/secrets/serviceAccountKey.json")
 
-	// 3. Firebase uygulamasını başlat
 	app, err := firebase.NewApp(ctx, nil, sa)
 	if err != nil {
 		log.Fatalf("HATA: Firebase başlatılamadı: %v", err)
 	}
 
-	// 4. Firestore servisine bağlanmayı dene
 	client, err := app.Firestore(ctx)
 	if err != nil {
 		log.Fatalf("HATA: Firestore veritabanına bağlanılamadı: %v", err)
 	}
 
-	// Bağlantı nesnesini işimiz bitince kapatmak için defer kullanıyoruz
 	defer client.Close()
 
-	// 5. Eğer buraya kadar hata almadıysak bağlantı nesnesi başarıyla oluşmuştur
 	log.Println("BAŞARILI: Firestore veritabanına bağlantı sağlandı!")
 }
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/mirackaansengull/mobilbackend
 
-go 1.25.3
+go 1.25.0
 
 require (
 	firebase.google.com/go/v4 v4.18.0
